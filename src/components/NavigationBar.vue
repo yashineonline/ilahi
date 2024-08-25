1a664fedf91852de99f2e96a9c3694743f4e9ac5
yashineonline <yashineonline@gmail.com>
2024-08-25 17:50:14 +0300

    Build

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,13 +2,13 @@
   <nav :class="['navbar shadow-lg', themeStore.theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-gray-800']" aria-label="Main navigation">
     <div class="container mx-auto px-4">
       <div class="flex-1">
-        <router-link to="/" class="btn btn-ghost normal-case text-xl" aria-label="Home">AQRT Sufi Songs</router-link>
+        <router-link to="/songs" class="btn btn-ghost btn-sm rounded-btn">View Ilahi List Here</router-link>        
       </div>
       <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
-          <li><router-link to="/about" class="btn btn-ghost btn-sm rounded-btn">About</router-link></li>
-          <li><router-link to="/songs" class="btn btn-ghost btn-sm rounded-btn">Ilahi List</router-link></li>
           <li><router-link to="/book" class="btn btn-primary btn-sm rounded-btn">Download Ilahi Book</router-link></li>
+          <li><router-link to="/about" class="btn btn-ghost btn-sm rounded-btn">About</router-link></li>
+          <li><router-link to="/" class="btn btn-ghost normal-case text-xl" aria-label="Home">Home</router-link></li>          
         </ul>
       </div>
       <button @click="refreshSongs" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
