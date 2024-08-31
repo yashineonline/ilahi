fdd7b3a049046988628cd420c8ab4a546f0553a3
aqrtsufi <ansariqadiririfai@gmail.com>
2024-08-31 16:35:22 +0300

    build for installation on phone with pwa sorted

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -6,33 +6,30 @@
       </div>
       <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
-          <li><router-link to="/book" class="btn btn-primary btn-sm rounded-btn">Download Ilahi Book</router-link></li>
-          <li><router-link to="/about" class="btn btn-ghost btn-sm rounded-btn">About Ilahi</router-link></li>
+          <li><router-link to="/book" class="btn btn-primary btn-sm">Download Ilahi Book</router-link></li>
+          <li><router-link to="/about" class="btn btn-ghost btn-sm">About Ilahi</router-link></li>
           <li><router-link to="/" class="btn btn-ghost normal-case text-xl" aria-label="Home">Home</router-link></li>          
         </ul>
       </div>
       <div class="flex items-center space-x-4">
-        <button @click="refreshSongs" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Refresh
-      </button>
-        <div class="icon-btn group relative" @click="handleIconClick('whatsapp')">
-          <img src="/whatsapp.jpeg" alt="Join Ilahi Classes" class="w-10 h-10" />
-        </div>
-        <div class="icon-btn group relative" @click="handleIconClick('youtube')">
-          <img src="/youtube.jpeg" alt="Play Ilahis" class="w-8 h-8" />
-        </div>
+        <button @click="refreshSongs" class="btn btn-ghost btn-circle">
+          <font-awesome-icon :icon="['fas', 'rotate']" spin style="color: #17a6ee;" />
+        </button>
+        <button class="btn btn-circle btn-outline" @click="handleIconClick('whatsapp')" aria-label="Join Ilahi Classes via WhatsApp">
+          <font-awesome-icon :icon="['fab', 'whatsapp']"  style="color: #63E6BE;" size="2xl" />
+        </button>
+        <button class="btn btn-circle btn-outline" @click="handleIconClick('youtube')" aria-label="Play Ilahis on YouTube">
+          <font-awesome-icon :icon="['fab', 'youtube']" style="color: #ff3d3d;" size="2xl" />
+        </button>
+        <Installation />
       </div>
       <!-- Large pop-up box -->
-      <div v-if="showPopup" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" @click="closePopup">
-        <div class="bg-white p-6 rounded-lg shadow-xl max-w-sm w-full m-4 relative" @click.stop>
-          <button @click="closePopup" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
+      <div v-if="showPopup" class="modal modal-open" @click="closePopup">
+        <div class="modal-box relative" @click.stop>
+          <button @click="closePopup" class="btn btn-sm btn-circle absolute right-2 top-2">✕</button>
           <h2 class="text-2xl font-bold mb-4">{{ popupContent.title }}</h2>
           <p class="mb-4">{{ popupContent.description }}</p>
-          <button @click="handlePopupAction" class="bg-white hover:bg-blue-500 text-blue-500 hover:text-white font-bold py-2 px-4 rounded w-full border border-blue-500 transition-colors duration-300">
+          <button @click="handlePopupAction" class="btn btn-primary btn-block">
             {{ popupContent.actionText }}
           </button>
         </div>
@@ -46,6 +43,7 @@ import { useThemeStore } from '../stores/themeStore'
 import { useSongStore } from '../stores/songStore'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import Installation from './Installation.vue'
 
 const themeStore = useThemeStore()
 const songStore = useSongStore()
@@ -90,76 +88,4 @@ const handlePopupAction = () => {
   popupContent.value.action()
   closePopup()
 }
-
-
 </script>
-
-<style scoped lang="postcss">
-.btn-lg {
-  @apply text-lg px-6 py-3 rounded-lg shadow-md;
-}
-
-.whatsapp-btn {
-  @apply relative flex items-center;
-}
-
-.whatsapp-text {
-  @apply absolute hidden group-hover:flex flex-col md:flex-row md:items-center md:ml-2 bg-white p-2 rounded shadow-md;
-  right: 0;
-  top: 100%;
-}
-
-@media (min-width: 768px) {
-  .whatsapp-text {
-    top: 50%;
-    right: 100%;
-    transform: translateY(-50%);
-  }
-}
-
-.whatsapp-text span {
-  display: block;
-  @media (min-width: 768px) {
-    display: inline;
-    margin-right: 0.25rem;
-  }
-}
-
-.icon-btn {
-  @apply relative inline-block cursor-pointer;
-}
-
-.hover-text {
-  @apply absolute hidden bg-white text-black p-2 rounded shadow-md text-sm whitespace-nowrap;
-  bottom: 100%;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-@media (min-width: 768px) {
-  .hover-text {
-    @apply group-hover:block;
-  }
-}
-
-.show-mobile {
-  @apply block;
-}
-
-/* For mobile */
-@media (max-width: 767px) {
-  .hover-text {
-    @apply mt-1 text-center;
-  }
-}
-
-/* For desktop */
-@media (min-width: 768px) {
-  .hover-text {
-    @apply absolute hidden whitespace-nowrap;
-    bottom: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-}
-</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,56 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
   base: '/ilahi/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['192.png', '48.png', '72.png', '96.png', '144.png', '512.png'],
+      manifest: {
+        name: 'Ilahi Book App',
+        short_name: 'Ilahi Book',
+        description: 'Explore Sacred Sufi Songs and Hymns',
+        theme_color: '#16a34a',
+        icons: [
+          {
+            src: '/48.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: '/72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: '/96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: '/144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: '/192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
