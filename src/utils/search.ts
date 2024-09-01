76682b780f13438b73bbef205b5d36f420ada130
aqrtsufi <ansariqadiririfai@gmail.com>
2024-09-01 18:54:33 +0300

    build with independent links now properly done with slugs

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,13 +1,19 @@
 import { SongData } from './types'; // Add this import
 
 function normalizeText(text: string): string {
+  const map: { [key: string]: string } = {
+    'ı': 'i', 'ğ': 'g', 'ü': 'u', 'ş': 's', 'ö': 'o', 'ç': 'c',
+    'İ': 'I','Î':'I' , 'Ğ': 'G', 'Ü': 'U', 'Ş': 'S', 'Ö': 'O', 'Ç': 'C',
+    'â': 'a', 'î': 'i', 'û': 'u'
+  };
   return text.toLowerCase()
-    .replace(/ı/g, 'i')
-    .replace(/ü/g, 'u')
-    .replace(/ö/g, 'o')
-    .replace(/ş/g, 's')
-    .replace(/ğ/g, 'g')
-    .replace(/ç/g, 'c');
+    .replace(/[ıİÎğĞüÜşŞöÖçÇâîû]/g, char => map[char] || char);
+}
+
+export function slugify(text: string): string {
+  return normalizeText(text)
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '');
 }
 
 export function searchSongs(songs: SongData[], query: string): SongData[] {
