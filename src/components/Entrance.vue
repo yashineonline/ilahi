4e5a6aceddc0517534da06c9e59808d37ed3e927
aqrtsufi <ansariqadiririfai@gmail.com>
2024-09-15 14:43:52 +0300

    build with button entrance enabled,

--- src/components/Entrance.vue
+++ src/components/Entrance.vue
@@ -4,7 +4,7 @@
         class="btn btn-success mb-4 w-full"
         @click="playWelcomeSequence"
         v-show="!isPlayingWelcome"
-        disabled
+        
       >
         Welcome
       </button>
@@ -12,7 +12,7 @@
         class="btn btn-secondary mb-4 w-full"
         @click="playFirstSegment"
         v-show="!isPlayingFirst"
-        disabled
+        
       >
         Walking to the Dergah
       </button>
@@ -21,7 +21,7 @@
         class="btn btn-secondary mb-4 w-full"
         @click="playSecondSegment"
         v-show="!isPlayingSecond"
-        disabled
+        
       >
         Entering the Dergah
       </button>
