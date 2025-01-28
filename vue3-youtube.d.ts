b2e193ed1154621609072c6265283f2e34068083
yashineonline <yashineonline@gmail.com>
2025-01-28 14:51:00 -0500

    commit with q

--- vue3-youtube.d.ts
+++ vue3-youtube.d.ts
@@ -1,1 +1,21 @@
 declare module 'vue3-youtube'
+
+declare namespace YT {
+    interface Player {
+      loadVideoById(options: { videoId: string; startSeconds: number; endSeconds: number }): void;
+      playVideo(): void;
+      getIframe(): HTMLIFrameElement;
+    }
+  
+    enum PlayerState {
+      ENDED = 0,
+      PLAYING = 1,
+      PAUSED = 2,
+      BUFFERING = 3,
+      CUED = 5
+    }
+  
+    interface OnStateChangeEvent {
+      data: PlayerState;
+    }
+  }
